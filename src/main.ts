d1198792555d01fde111e98a3a2e2d03f7d61036
aleRiki <alericardo826@gmail.com>
2025-10-18 07:03:49 +0200

    comit

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',//process.env.ENV_ORIGIN , 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
-  });
+  // app.enableCors({
+  //   origin: '*',//process.env.ENV_ORIGIN , 
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true, 
+  // });
   app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
